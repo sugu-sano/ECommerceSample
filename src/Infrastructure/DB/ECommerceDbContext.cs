76d8fe69ed68c2d6bc0f5d12ef80ed1cc0fd88ab
sugu-sano <sugu-sano@i-mobile.co.jp>
2021-03-23 02:28:54 +0900

    refs #32 EFコードファーストのための基底テーブルクラスを作成する

---
+++ src/Infrastructure/DB/ECommerceDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace ECommerceSample.Infrastructure.DB
+{
+    /// <summary>
+    /// EコマースDBコンテキスト
+    /// </summary>
+    public class ECommerceDbContext : DbContext
+    {
+        /// <summary>
+        /// EコマースDBコンテキスト
+        /// </summary>
+        /// <param name="options">DBコンテキストオプション</param>
+        public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options) : base(options) { }
+
+        /// <summary>
+        /// モデル構成
+        /// </summary>
+        /// <param name="builder">モデルビルダー</param>
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ src/Infrastructure/DB/Tables/TableBaseConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECommerceSample.Infrastructure.DB.Tables
+{
+    /// <summary>
+    ///  基底テーブルの設定
+    /// </summary>
+    internal class TableBaseConfiguration : IEntityTypeConfiguration<TableBase>
+    {
+        /// <summary>
+        /// 基底テーブルを構成する
+        /// </summary>
+        /// <param name="builder">基底テーブルの構成ビルダー</param>
+        public void Configure(EntityTypeBuilder<TableBase> builder)
+        {
+            builder.HasKey(tableBase => tableBase.Id);
+
+            builder.Property(tableBase => tableBase.CreateAt)
+                .IsRequired(true);
+
+            builder.Property(tableBase => tableBase.UpdatedAt)
+                .IsRequired(false);
+        }
+    }
+}

8ad701482b3bd8fc7f7294bfe2e9d25e282385c5
nossa <sa.50.00.no.riku+github@gmail.com>
2021-03-31 23:58:46 +0900

    TypeScript設定

--- src/web-front/pages/_app.tsx
+++ src/web-front/pages/_app.tsx
@@ -1,14 +1,16 @@
-import "../styles/globals.css";
+import '../styles/globals.css';
+import React from 'react';
+import type { AppProps } from 'next/app'
 
-function SafeHydrate({ children }) {
+const SafeHydrate: React.FC = ({ children }) => {
   return (
     <div suppressHydrationWarning={true}>
       {typeof window === "undefined" ? null : children}
     </div>
   );
-}
+};
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SafeHydrate>
       <Component {...pageProps} />

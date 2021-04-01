818f8ee5cb5e9803dbf5a07651d166a63677447c
nossa <sa.50.00.no.riku+github@gmail.com>
2021-04-02 00:55:03 +0900

    ESLintの設定の追加

---
+++ src/web-front/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  ignorePatterns: ['!.eslintrc.js', '!.prettierrc.js'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'react'],
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/prop-types': 'off',
+  },
+}
